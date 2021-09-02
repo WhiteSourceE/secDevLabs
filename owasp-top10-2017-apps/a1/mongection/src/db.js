eb130f0541521864b590477fc2ca340395a46985
denisjovic <denisjovic@pm.me>
2021-09-02 11:12:43 +0200

    Sanitized with mongo-sanitize library

--- owasp-top10-2017-apps/a1/mongection/src/db.js
+++ owasp-top10-2017-apps/a1/mongection/src/db.js
@@ -1,52 +1,58 @@
 const User = require('./models/user');
+var sanitize = require('mongo-sanitize');
 
 const register = async (user) => {
+  try {
+    const { name, email, password } = user;
+    const s_name = sanitize(name);
+    const s_email = sanitize(email);
+    const s_password = sanitize(password);
 
-    try { 
-        const { name, email, password } = user;
+    const existUser = await User.findOne({ email: s_email });
 
-        const existUser = await User.findOne({email: email});
-
-        if(existUser) { return null }
-
-        const newUser = new User({
-            name: name,
-            email: email,
-            password: password
-        });
-
-        await newUser.save();
-
-        return newUser;
+    if (existUser) {
+      return null;
     }
 
-    catch(error) { throw error; }
-    
-}
+    const newUser = new User({
+      name: s_name,
+      email: s_email,
+      password: s_password,
+    });
+
+    await newUser.save();
+
+    return newUser;
+  } catch (error) {
+    throw error;
+  }
+};
 
 const login = async (credentials) => {
+  try {
+    const { email, password } = credentials;
+    const s_email = sanitize(email);
+    const s_password = sanitize(password);
 
-    try {
-        const { email, password } = credentials;
+    const existsUser = await User.find({
+      $and: [{ email: s_email }, { password: s_password }],
+    });
 
-        const existsUser = await User.find({$and: [ { email: email}, { password: password} ]});
-
-        if(!existsUser) { return null;}
-
-        const returnUser = existsUser.map((user) => {
-            return user.email
-        })
-
-
-        return returnUser;
+    if (!existsUser) {
+      return null;
     }
 
-    catch(error) { throw error; }
-    
+    const returnUser = existsUser.map((user) => {
+      return user.email;
+    });
 
-}
+    return returnUser;
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
-    register,
-    login
+  register,
+  login,
 };
