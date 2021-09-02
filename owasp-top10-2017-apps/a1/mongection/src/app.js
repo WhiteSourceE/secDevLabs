44c48fa3804273bc9d687488981e6705436ea64d
denisjovic <denisjovic@pm.me>
2021-09-02 11:41:59 +0200

    Sanitized app.js with mongo-sanitize

--- owasp-top10-2017-apps/a1/mongection/src/app.js
+++ owasp-top10-2017-apps/a1/mongection/src/app.js
@@ -3,83 +3,91 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
 const db = require('./db');
+var sanitize = require('mongo-sanitize');
 
 const server = express();
 
 const PORT = 10001;
 
-var emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+var emailRegex =
+  /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
 
 server.use(bodyParser.json());
 
-server.use(function(request, response, next) {
-    response.header("Access-Control-Allow-Origin", "http://localhost:10001");
-    response.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  });
+server.use(function (request, response, next) {
+  response.header('Access-Control-Allow-Origin', 'http://localhost:10001');
+  response.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 server.use(express.static(__dirname + '/public'));
 
-server.get("/", (request, response) => {
-    response.sendFile(path.join(__dirname+'/public/view/index.html'));
+server.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname + '/public/view/index.html'));
 });
 
-server.get("/login.html", (request, response) => {
-    response.sendFile(path.join(__dirname+'/public/view/login.html'));
+server.get('/login.html', (request, response) => {
+  response.sendFile(path.join(__dirname + '/public/view/login.html'));
 });
 
-server.get("/register.html", (request, response) => {
-    response.sendFile(path.join(__dirname+'/public/view/register.html'));
+server.get('/register.html', (request, response) => {
+  response.sendFile(path.join(__dirname + '/public/view/register.html'));
 });
 
-server.post("/login", async (request, response) => {
+server.post('/login', async (request, response) => {
+  try {
+    const email = sanitize(request.body.email);
+    const password = sanitize(request.body.password);
 
-    try {
+    const user = await db.login({ email, password });
 
-        const email = request.body.email;
-        const password = request.body.password;
+    console.log(user.length);
 
-        const user = await db.login({email, password});
-
-        console.log(user.length)
-
-        if(user.length == 0) { response.send('Bad Credentials'); }
-
-        response.send("<h1>Hello, Welcome Again!</h1><h3>" + user + "</h3>");
-    }
-   
-    catch(error) { throw error; }
-
-
-});
-
-server.post("/register", async (request, response) => {
-    try {
-        const name = request.body.name;
-        const email = request.body.email;
-        const password = request.body.password;
-
-        const validEmail = emailRegex.test(email);
-        if(!validEmail) { response.send('Bad Email'); }
-
-        else {
-            const user = await db.register({name, email, password});
-
-            if(!user) { response.send('User Already Exists'); }
-
-            response.send("<h1>Welcome to Mongection System</h1><h3>" + user.email + "</h3>");
-        }
-        
+    if (user.length == 0) {
+      response.send('Bad Credentials');
     }
 
-    catch(error) { throw error; }
-
+    response.send('<h1>Hello, Welcome Again!</h1><h3>' + user + '</h3>');
+  } catch (error) {
+    throw error;
+  }
 });
 
-mongoose.connect(`mongodb://${process.env.DBUSER}:${process.env.DBPASS}@mongo:27017/mongection`, {useNewUrlParser: true})
-    .then( () => {
-        console.log('Server Running at port: ' + PORT);
+server.post('/register', async (request, response) => {
+  try {
+    const name = sanitize(request.body.name);
+    const email = sanitize(request.body.email);
+    const password = sanitize(request.body.password);
 
-        server.listen(PORT);
-    })
-    .catch( error => { throw error; });
+    const validEmail = emailRegex.test(email);
+    if (!validEmail) {
+      response.send('Bad Email');
+    } else {
+      const user = await db.register({ name, email, password });
+
+      if (!user) {
+        response.send('User Already Exists');
+      }
+
+      response.send(
+        '<h1>Welcome to Mongection System</h1><h3>' + user.email + '</h3>'
+      );
+    }
+  } catch (error) {
+    throw error;
+  }
+});
+
+mongoose
+  .connect(
+    `mongodb://${process.env.DBUSER}:${process.env.DBPASS}@mongo:27017/mongection`,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log('Server Running at port: ' + PORT);
+
+    server.listen(PORT);
+  })
+  .catch((error) => {
+    throw error;
+  });
